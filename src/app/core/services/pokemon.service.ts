467e30ef71f8bd33646cd264fc15016cb514c9e3
miguel.roldan <miguel.roldan@pragma.com.co>
2025-06-15 20:22:44 -0500

    feature: angular base repo created

---
+++ src/app/core/services/pokemon.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PokemonListResponse } from '../../interfaces/pokemon';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  constructor(private http: HttpClient) {}
+
+  getPokemonList(): Observable<PokemonListResponse> {
+    return this.http.get<PokemonListResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+    );
+  }
+}
