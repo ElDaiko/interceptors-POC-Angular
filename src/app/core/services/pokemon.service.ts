a9f943f8fb75fed840c9933184099cc97097ae06
miguel.roldan <miguel.roldan@pragma.com.co>
2025-06-15 21:49:01 -0500

    feature: error message component added

--- src/app/core/services/pokemon.service.ts
+++ src/app/core/services/pokemon.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { PokemonListResponse } from '../../interfaces/pokemon';
 
 @Injectable({
@@ -10,8 +10,23 @@ export class PokemonService {
   constructor(private http: HttpClient) {}
 
   getPokemonList(): Observable<PokemonListResponse> {
-    return this.http.get<PokemonListResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
-    );
+    return this.http
+      .get<PokemonListResponse>(
+        'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0aa'
+      )
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          let errorMessage = '';
+          if (error.error instanceof ErrorEvent) {
+            errorMessage = `Error: ${error.error.message}`;
+          } else {
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+          }
+
+          return throwError(() => {
+            new Error(errorMessage);
+          });
+        })
+      );
   }
 }
--- src/app/pages/pokemon-list/pokemon-list.component.ts
+++ src/app/pages/pokemon-list/pokemon-list.component.ts
@@ -1,22 +1,29 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../../core/services/pokemon.service';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, Observable } from 'rxjs';
 import { PokemonListResponse } from '../../interfaces/pokemon';
 import { PokemonItemComponent } from '../../components/pokemon-item/pokemon-item.component';
+import { ErrorMessageComponent } from '../../components/error-message/error-message.component';
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [AsyncPipe, PokemonItemComponent],
+  imports: [AsyncPipe, PokemonItemComponent, ErrorMessageComponent],
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent implements OnInit {
   public pokemonResults$!: Observable<PokemonListResponse>;
+  public errorMessage!: string;
   constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
-    this.pokemonResults$ = this.pokemonService.getPokemonList();
+    this.pokemonResults$ = this.pokemonService.getPokemonList().pipe(
+      catchError((error: string) => {
+        this.errorMessage = error;
+        return EMPTY;
+      })
+    );
   }
 }
