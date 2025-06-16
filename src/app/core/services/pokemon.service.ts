12bf0140bf1dd80495ab0de31d33b7d287168442
miguel.roldan <miguel.roldan@pragma.com.co>
2025-06-16 10:10:38 -0500

    feature: interceptor added to http requests

--- src/app/core/services/pokemon.service.ts
+++ src/app/core/services/pokemon.service.ts
@@ -10,23 +10,8 @@ export class PokemonService {
   constructor(private http: HttpClient) {}
 
   getPokemonList(): Observable<PokemonListResponse> {
-    return this.http
-      .get<PokemonListResponse>(
-        'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0aa'
-      )
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          let errorMessage = '';
-          if (error.error instanceof ErrorEvent) {
-            errorMessage = `Error: ${error.error.message}`;
-          } else {
-            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-          }
-
-          return throwError(() => {
-            new Error(errorMessage);
-          });
-        })
-      );
+    return this.http.get<PokemonListResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0aa'
+    );
   }
 }
